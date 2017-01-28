3f0c2b3a28ae24ca46f9a030e5246e01b0b30943
angujo <angujomondi@gmail.com>
2017-01-29 02:02:58 +0300

    db

---
+++ dev.sql
@@ -0,0 +1,139 @@
+/*
+SQLyog Ultimate v12.2.1 (64 bit)
+MySQL - 5.7.14-log : Database - dev
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dev` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dev`;
+
+/*Table structure for table `modules` */
+
+DROP TABLE IF EXISTS `modules`;
+
+CREATE TABLE `modules` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `description` varchar(450) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `identifier` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `organization_modules` */
+
+DROP TABLE IF EXISTS `organization_modules`;
+
+CREATE TABLE `organization_modules` (
+  `organization_id` int(10) unsigned NOT NULL,
+  `module_id` int(10) unsigned NOT NULL,
+  KEY `organization_id` (`organization_id`),
+  KEY `module_id` (`module_id`),
+  CONSTRAINT `organization_modules_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`),
+  CONSTRAINT `organization_modules_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `organizations` */
+
+DROP TABLE IF EXISTS `organizations`;
+
+CREATE TABLE `organizations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `address` varchar(450) DEFAULT NULL,
+  `telephone1` varchar(20) NOT NULL,
+  `telephone2` varchar(20) DEFAULT NULL,
+  `email` varchar(75) NOT NULL,
+  `email2` varchar(75) DEFAULT NULL,
+  `website` varchar(50) DEFAULT NULL,
+  `subdomain` varchar(50) DEFAULT NULL COMMENT 'For custom connection',
+  `auth_number` varchar(50) NOT NULL COMMENT 'Identification number for users',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sessions` */
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(30) NOT NULL,
+  `organization_id` int(11) DEFAULT NULL,
+  `browser` varchar(55) DEFAULT NULL,
+  `ip_address` varchar(20) NOT NULL,
+  `office_id` int(11) DEFAULT NULL,
+  `started` datetime DEFAULT NULL,
+  `ended` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `trash` */
+
+DROP TABLE IF EXISTS `trash`;
+
+CREATE TABLE `trash` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `data` text,
+  `name` varchar(65) DEFAULT NULL,
+  `dated` datetime DEFAULT NULL,
+  `actionby` int(11) DEFAULT NULL,
+  `table_name` varchar(65) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_modules` */
+
+DROP TABLE IF EXISTS `user_modules`;
+
+CREATE TABLE `user_modules` (
+  `user_id` int(10) unsigned NOT NULL,
+  `module_id` int(10) unsigned NOT NULL,
+  `session_id` int(10) unsigned NOT NULL,
+  `started` datetime DEFAULT NULL,
+  `ended` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_organizations` */
+
+DROP TABLE IF EXISTS `user_organizations`;
+
+CREATE TABLE `user_organizations` (
+  `user_id` int(10) unsigned NOT NULL,
+  `organization_id` int(10) unsigned NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(60) NOT NULL,
+  `last_name` varchar(60) NOT NULL,
+  `surname` varchar(60) DEFAULT NULL,
+  `profile_picture` varchar(45) DEFAULT NULL,
+  `auth_key` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`,`username`,`auth_key`)
+) ENGINE=InnoDB AUTO_INCREMENT=245 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
